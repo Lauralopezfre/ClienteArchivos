9a89b532ea236297b9c866daad7bcf618db33cef
Lauralopezfre <15325051520023@cetis108.edu.mx>
2021-02-28 12:01:52 -0700

    Falta el manejo de archivos en vez del mensaje en Cliente.java

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -5,6 +5,13 @@
  */
 package cliente;
 
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import javax.imageio.IIOException;
+
 /**
  *
  * @author laura
@@ -14,8 +21,54 @@ public class Cliente {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    private static final int TIMEOUT = 3000;
+    private static final int INTENTOS = 5;
+    
+    public static void main(String[] args) throws IOException{
+        if((args.length < 2)||(args.length > 3)){
+            throw new IllegalArgumentException("Parameter(s): <Server> <Archivo> [<Port>]");
+        }
+        
+        //Se solicita la dirección
+        InetAddress address = InetAddress.getByName(args[0]);
+        
+        //Aqui es donde vamos a mandar el archivo
+        byte[] archivo = args[1].getBytes();
+        
+        //Aqui se solicita el puerto
+        int port = (args.length == 3) ? Integer.parseInt(args[2]) : 7;
+        
+        DatagramSocket datagrama = new  DatagramSocket();
+        
+        datagrama.setSoTimeout(TIMEOUT);
+        
+        DatagramPacket enviar = new DatagramPacket(archivo, archivo.length, address, port);
+        DatagramPacket recibir = new DatagramPacket(new byte[archivo.length], archivo.length);
+        
+        int tries = 0;
+        boolean respuestaRecibido = false;
+        do{
+            datagrama.send(enviar);
+            try{
+                datagrama.receive(recibir);
+                
+                if(!recibir.getAddress().equals(address)){
+                    throw new IIOException("No se supo de quien se recibio");
+                }
+                respuestaRecibido = true;
+            }catch(InterruptedIOException e){
+                tries++;
+                System.out.println("Intentos " + (INTENTOS - tries));
+            }
+        }while((!respuestaRecibido)&&(tries < INTENTOS));
+        
+        if(respuestaRecibido){
+            System.out.println("Recibido " + new String(recibir.getData()));
+        }else{
+            System.out.println("No responde");
+        }
+        datagrama.close();
     }
     
 }
