283b941deed4b90aaa175f0b19c911f6b3bb5f59
Lauralopezfre <15325051520023@cetis108.edu.mx>
2021-03-04 11:31:35 -0700

    Ya muestra el mensaje

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -22,66 +22,66 @@ public class Cliente {
     /**
      * @param args the command line arguments
      */
-    
     private static final int TIMEOUT = 3000;
     private static final int INTENTOS = 5;
-    
-    public static void main(String[] args) throws IOException{
-        Scanner in=new Scanner(System.in);
-        
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
         //Se solicita la dirección
         InetAddress address = InetAddress.getByName("127.0.0.1");
-        
+
         System.out.print("Nombre del archivo que quieres: ");
-        String nombre=in.next();
-        
+        String nombre = in.next();
+
         byte[] archivo = (nombre).getBytes();
-        
-                
+
         //Aqui se solicita el puerto
         int port = 7171;
-        
-        DatagramSocket datagrama = new  DatagramSocket();
-        
+
+        DatagramSocket datagrama = new DatagramSocket();
+
         datagrama.setSoTimeout(TIMEOUT);
-        
+
         DatagramPacket enviar = new DatagramPacket(archivo, archivo.length, address, port);
         DatagramPacket recibir = new DatagramPacket(new byte[255], 255);
-        
+
         int tries = 0;
         boolean respuestaRecibido = false;
         String mensaje = "";
-        do{
+        do {
             datagrama.send(enviar);
-            
-            try{
+
+            try {
                 datagrama.receive(recibir);
-                
+
                 byte k = 1;
-                if(recibir.getData().equals(k) ) {
-                    
+                byte[] ks = recibir.getData();
+
+                if (ks[0] == k) {
+
                     datagrama.receive(recibir);
                     System.out.println(new String(recibir.getData()));
                     mensaje += new String(recibir.getData());
                     k++;
                 }
-                
-                if(!recibir.getAddress().equals(address)){
+
+                if (!recibir.getAddress().equals(address)) {
                     throw new IIOException("No se supo de quien se recibio");
                 }
                 respuestaRecibido = true;
-            }catch(InterruptedIOException e){
+            } catch (InterruptedIOException e) {
                 tries++;
                 System.out.println("Intentos " + (INTENTOS - tries));
             }
-        }while((!respuestaRecibido)&&(tries < INTENTOS)&& recibir.getData().equals(0));
-        
-        if(respuestaRecibido){
+        } while ((!respuestaRecibido) && (tries < INTENTOS) && recibir.getData().equals(0));
+
+        if (respuestaRecibido) {
             System.out.println("Contenido: " + mensaje);
-        }else{
+        } else {
             System.out.println("No responde");
         }
         datagrama.close();
     }
-    
+
 }
